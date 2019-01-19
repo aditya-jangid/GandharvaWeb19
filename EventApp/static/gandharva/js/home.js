e23f7de62d91241dcf21332ba66045f8c3fb703b
Sanul-Raskar <sanulraskar@gmail.com>
2019-01-19 19:29:21 +0530

    Final Coming Soon page to deploy

--- EventApp/static/gandharva/js/home.js
+++ EventApp/static/gandharva/js/home.js
@@ -2,46 +2,46 @@ console.log('Hello');
 
 sal();
 
-$(document).ready(function () {
-    $(".testimonial-carousel").slick({
-        infinite: !0,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        autoplay: !1,
-        arrows: true,
-        prevArrow: $(".testimonial-carousel-controls .prev"),
-        nextArrow: $(".testimonial-carousel-controls .next"),
-        responsive: [{
-            breakpoint: 1200,
-            settings: {
-                slidesToShow: 3
-            }
-        }, {
-            breakpoint: 992,
-            settings: {
-                slidesToShow: 2
-            }
-        }, {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 1
-            }
-        }]
-    });
-});
+// $(document).ready(function () {
+//     $(".testimonial-carousel").slick({
+//         infinite: !0,
+//         slidesToShow: 4,
+//         slidesToScroll: 1,
+//         autoplay: !1,
+//         arrows: true,
+//         prevArrow: $(".testimonial-carousel-controls .prev"),
+//         nextArrow: $(".testimonial-carousel-controls .next"),
+//         responsive: [{
+//             breakpoint: 1200,
+//             settings: {
+//                 slidesToShow: 3
+//             }
+//         }, {
+//             breakpoint: 992,
+//             settings: {
+//                 slidesToShow: 2
+//             }
+//         }, {
+//             breakpoint: 600,
+//             settings: {
+//                 slidesToShow: 1
+//             }
+//         }]
+//     });
+// });
+//
 
-
-$(document).ready(function () {
-    // Transition effect for navbar
-    $(window).scroll(function () {
-        // checks if window is scrolled more than 500px, adds/removes solid class
-        if ($(this).scrollTop() > 500) {
-            $('.navbar').addClass('solid');
-        } else {
-            $('.navbar').removeClass('solid');
-        }
-    });
-});
+// $(document).ready(function () {
+//     // Transition effect for navbar
+//     $(window).scroll(function () {
+//         // checks if window is scrolled more than 500px, adds/removes solid class
+//         if ($(this).scrollTop() > 500) {
+//             $('.navbar').addClass('solid');
+//         } else {
+//             $('.navbar').removeClass('solid');
+//         }
+//     });
+// });
 
 particlesJS('particles-js',
 
