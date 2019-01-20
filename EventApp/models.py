f9a2a3c0d14e46eff06a92f9b3c17726896a3caf
Kushal <kushshah777888@gmail.com>
2019-01-20 14:15:30 +0530

    Changed Sponsor name

--- EventApp/models.py
+++ EventApp/models.py
@@ -47,8 +47,8 @@ class EventMaster(models.Model):
     team_size = models.IntegerField()
     entry_fee = models.IntegerField()
     objective = models.CharField(max_length=1000, blank=True)
-    rounds = models.CharField(max_length=1000, blank=True)
-    rules = models.CharField(max_length=1000, blank=True)
+    rounds = models.CharField(max_length=10000, blank=True)
+    rules = models.CharField(max_length=100000, blank=True)
 
     def __str__(self):
         return self.event_name
